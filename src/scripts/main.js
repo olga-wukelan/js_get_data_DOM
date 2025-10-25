29cf5eafafecdc3e1eae864f2c1205fdbe1e26d5
Olga Korynenko <olga.korynenko@gmail.com>
2025-10-25 12:00:38 +0400

    create a solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-// write your code here
+const populationElements = document.querySelectorAll('.population');
+const populationStrings = Array.from(populationElements).map((element) => {
+  return element.textContent;
+});
+
+const numbersAll = populationStrings.map((str) => {
+  const cleanStr = str.replace(/,/g, '');
+
+  return parseFloat(cleanStr);
+});
+
+const total = numbersAll.reduce((current, acc) => current + acc, 0);
+const average = Math.floor(
+  numbersAll.length > 0 ? total / numbersAll.length : 0,
+);
+
+const totalPopulation = document.querySelector('.total-population');
+const averagePopulation = document.querySelector('.average-population');
+
+totalPopulation.textContent = total;
+averagePopulation.textContent = average;
